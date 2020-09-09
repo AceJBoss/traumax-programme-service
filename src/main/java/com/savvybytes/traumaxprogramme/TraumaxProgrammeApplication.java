d08a7e0a8cab4a0d8e27a27e72b464bed7ff48d2
JBoss <abolade.akintomiwa@gmail.com>
2020-09-09 14:25:16 +0100

    Application Properties Setup

---
+++ src/main/java/com/savvybytes/traumaxprogramme/TraumaxProgrammeApplication.java
@@ -0,0 +1,13 @@
+package com.savvybytes.traumaxprogramme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraumaxProgrammeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraumaxProgrammeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/savvybytes/traumaxprogramme/TraumaxProgrammeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.savvybytes.traumaxprogramme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraumaxProgrammeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
